d0deb1beea9cf9ad9a14d3fb9d8d51fd3f7c9257
Bastiaan Nijkamp <bastiaan@wesnijk.nl>
2023-12-01 10:49:26 +0100

    Initial Kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "nl.bastiaan"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
