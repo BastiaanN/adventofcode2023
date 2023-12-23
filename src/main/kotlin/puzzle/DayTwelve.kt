ff2f16dd49155c9c3c3759846ccb86e35d05bb2f
BastiaanN <bastiaan@wesnijk.nl>
2023-12-23 12:08:09 +0100

    Day 12 WIP

--- src/main/kotlin/puzzle/DayTwelve.kt
+++ src/main/kotlin/puzzle/DayTwelve.kt
@@ -40,63 +40,68 @@ class DayTwelve : Puzzle {
     private fun calculatePossibilitiesForLine(line: String): Long {
         // Get rid of extra "."
         val lineParts = line.replace("\\.{2,}".toRegex(), ".").split(" ")
-        val arrangements = lineParts[1].split(",").map { s -> s.toInt() }
+        val arrangements = lineParts[1].split(",").map { s -> s.toInt() }.toTypedArray()
 
         // We need to calculate...
-        return calculatePossibilities(lineParts[0].trim { c -> c == '.' }, arrangements)
+        return calculatePossibilities(lineParts[0].trim { c -> c == '.' }, arrangements, arrangements.sum())
     }
 
-    private fun calculatePossibilities(segment: String, arrangements: List<Int>): Long {
+    private fun calculatePossibilities(segment: String, arrangements: Array<Int>, arrangementSum: Int): Long {
         var possibilities = 0L
 
-      //  println("Trying segment: ${segment}")
+//        println("Trying segment: ${segment}")
 
         // Check if there is still a chance this ever becomes a valid arrangement.
         var arrangementIdx = 0
         var hashLength = 0
+        var numPossibleHashes = 0
+        var foundQuestionMark = false
         for (segChar in segment) {
 
-            // If there still is a place to fill in we can stop as we are not
+            // If there still is a place to fill in we can probably stop as we are not
             // smart enough to predict the rest...
-            if(segChar == '?') {
-                // We can try to be a liiiitttle smarter...
-                // Not enough #/? to get the total arrangement size. So we can stop this flow too.
-//                if(segment.count { c -> c == '#' || c == '?'} < arrangements.sum()) return 0
-                break
-            }
+            if (segChar == '?') foundQuestionMark = true
+            if (segChar == '#' || segChar == '?') numPossibleHashes++
 
             // The number of hashes in this part should equal the expected number of arrangements. If not this would
             // always lead to an impossible combination.
-            if(segChar == '#') hashLength++
-            else if(hashLength > 0) {
-                if(arrangementIdx >= arrangements.size || hashLength != arrangements[arrangementIdx++]) return 0
-                // Reset hashLength.
-                hashLength = 0
+            if(foundQuestionMark) {
+                if (numPossibleHashes > arrangementSum) break
+            } else {
+                if (segChar == '#') hashLength++
+                else if (hashLength > 0) {
+                    if (arrangementIdx >= arrangements.size || hashLength != arrangements[arrangementIdx++]) return 0
+                    // Reset hashLength.
+                    hashLength = 0
+                }
             }
         }
 
+        // Not enough #/? to get the total arrangement size? Then we stop this flow too.
+        if(arrangementSum > numPossibleHashes) return 0
+
         val mutatedSegmentWithDot = segment.replaceFirst('?', '.')
-        if (isValidSegment(mutatedSegmentWithDot, arrangements)) {
+        if (isValidSegment(mutatedSegmentWithDot, arrangements, arrangementSum)) {
             possibilities++
         } else if (mutatedSegmentWithDot.contains('?')) {
-            possibilities += calculatePossibilities(mutatedSegmentWithDot, arrangements)
+            possibilities += calculatePossibilities(mutatedSegmentWithDot, arrangements, arrangementSum)
         }
 
         val mutatedSegmentWithHash = segment.replaceFirst('?', '#')
-        if (isValidSegment(mutatedSegmentWithHash, arrangements)) {
+        if (isValidSegment(mutatedSegmentWithHash, arrangements, arrangementSum)) {
             possibilities++
         } else if (mutatedSegmentWithHash.contains('?')) {
-            possibilities += calculatePossibilities(mutatedSegmentWithHash, arrangements)
+            possibilities += calculatePossibilities(mutatedSegmentWithHash, arrangements, arrangementSum)
         }
 
         return possibilities
     }
 
-    private fun isValidSegment(segment: String, arrangements: List<Int>): Boolean {
+    private fun isValidSegment(segment: String, arrangements: Array<Int>, arrangementSum: Int): Boolean {
         // If contains ? we are not done yet.
         if (segment.contains("?")) return false
         // Not same
-        if (segment.count { c -> c == '#' } != arrangements.sum()) return false
+        if (segment.count { c -> c == '#' } != arrangementSum) return false
 
         val splittedSegments = segment.split(".").filter { s -> s.isNotEmpty() }
         
